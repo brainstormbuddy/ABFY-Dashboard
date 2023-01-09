bb63025535fc38b176534dd2507d1a82ae4d0bf4
smartfox1203 <adam@clearlyinnovative.com>
2023-01-09 07:36:22 +0200

    update the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -80,7 +80,7 @@ export default function Login() {
         {mdUp && (
           <SectionStyle>
             <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-              Hi, Welcome Back
+              Hi, Welcome
             </Typography>
             <img src="/static/illustrations/illustration_login.png" alt="login" />
           </SectionStyle>
